99e29a2bfb9bf5b418f91fe9d81ec8d6f78ee2b8
maguea <121208615+maguea@users.noreply.github.com>
2023-03-21 20:14:48 -0400

    Update pemdas.py

--- pemdas.py
+++ pemdas.py
@@ -1,3 +1,18 @@
+#Pemdas
+#Terminal calculator that solves basic equations in PEMDAS order
+#Instructions: Program will ignore white space, and correct minor misinputs.
+#Program Syntax:
+#   '(' to begin a parnethesis, ')' to close parenthesis 
+#   '^' for power of
+#   '*' for multiplication, and '/' for division
+#   '+' for addition, and '-' for subtraction
+#   '.' to add a decimal point
+#   To take a root, use the power rule (e.g. square root of 4 is written as "4^(1/2)")
+#No support for Euler's number (e) or logs
+#Hit enter after typing equation (you can include or omit '=' sign)
+
+#-----------code below-----------#
+
 #Takes input
 def start():
     initialEq = input("Enter equation (Type \"EXIT\" to close program): ")
@@ -13,13 +28,12 @@ def breakUp(initialEq):
     oParCnt = listed.count('(')
     cParCnt = listed.count(')')
     for x in listed:
-        if isOp(x) == False:
-            if(x != '(') and (x != ')') and (x!='.'):
-                try:
-                    float(x)
-                except:
-                    print("\"" + str(x) + "\" has been automatically removed.")
-                    listed.remove(x)
+        if (isOp(x) == False) and (x != '(') and (x != ')') and (x!='.'):
+            try:
+                float(x)
+            except:
+                print("\"" + str(x) + "\" has been automatically removed.")
+                listed.remove(x)
     if not listed:
         print("No equation left.")
     else:
@@ -91,7 +105,8 @@ def delimit(listed):
     x+=1
     while x < len(listed):
         if isOp(listed[x]):
-            nums.append(float(tempHold))
+            finalFloat = floatCheck(tempHold)
+            nums.append(finalFloat)
             tempHold = ''
             if (listed[x+1] == '+') | (listed[x+1] == '*') | (listed[x+1] == '/') | (listed[x+1] == '^'):
                 print("Extra operator: \"" + listed[x+1] + "\" has been found, automatically removed.")
@@ -103,11 +118,29 @@ def delimit(listed):
                 x+=1    
         tempHold += listed[x]
         x+=1
-    nums.append(float(tempHold))
+    nums.append(floatCheck(tempHold))
     return(nums, sigs)
+
+#final check to limit number input error
+def floatCheck(input):
+    try:
+        float(input)
+    except:
+        pointRep = input.count('.')
+        minusRep = input.count('-')
+        if pointRep > 1:
+            while pointRep > 0:
+                input = input[1:]
+                pointRep -= 1
+        if minusRep > 1:
+            while minusRep > 0:
+                input = input[1:]
+                minusRep -= 1
+    return(float(input))
 
 #Checks operator list for mixed order
 def eqCheck(nums, sigs):
+    error0 = False
     newNum = list()
     exp = sigs.count('^')
     muDi = sigs.count('*') + sigs.count('/')
@@ -126,7 +159,6 @@ def eqCheck(nums, sigs):
             newNum.clear()
         x = 0
         newNum.clear()
-        #REWRITE EQCHECK TRY FOR X IN LIST
         while muDi > 0:
             if sigs.count('*') > 0:
                 x = sigs.index('*')
@@ -135,6 +167,9 @@ def eqCheck(nums, sigs):
             muDi-=1
             newNum.extend((nums[x], nums[x+1]))
             total = mulDi(newNum, sigs[x])
+            if total == 'Cannot divide by 0, try again.':
+                return('Cannot divide by 0, try again.')
+                break
             nums.pop(x+1)
             nums[x] = total
             sigs.pop(x)
@@ -165,7 +200,10 @@ def adSub(nums, sigs):
 #Calculate multiplication and division in order
 def mulDi(nums, sigs):
     if sigs[0] == '/':
-        total = nums[0] / nums[1]
+        if nums[1] != 0:
+            total = nums[0] / nums[1]
+        else:
+            return('Cannot divide by 0, try again.')
     if sigs[0] == '*':
         total = nums[0] * nums[1]
     return(total)
